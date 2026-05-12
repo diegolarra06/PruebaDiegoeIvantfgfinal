556f6cd6ebdc4b83b3e8180863e72ad7cf973730
diego <diego@larrazabal40>
2026-05-12 19:08:30 +0200

    first commit

---
+++ backend/AdoptaUnCompaneroApplication-3/src/main/java/com/daw/adoptauncompanero/AdoptaUnCompaneroApplication3Application.java
@@ -0,0 +1,13 @@
+package com.daw.adoptauncompanero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptaUnCompaneroApplication3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptaUnCompaneroApplication3Application.class, args);
+	}
+
+}
---
+++ backend/AdoptaUnCompaneroApplication-3/src/test/java/com/daw/adoptauncompanero/AdoptaUnCompaneroApplication3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.adoptauncompanero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptaUnCompaneroApplication3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
